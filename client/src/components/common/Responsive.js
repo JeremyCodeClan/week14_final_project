c87cb20d73ce2a2e74676c486fcca234421a8dd5
Jeremy Yoo <jyyoo824@gmail.com>
2021-10-20 00:49:32 +0100

    fixed responsive values

--- client/src/components/common/Responsive.js
+++ client/src/components/common/Responsive.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ResponsiveBlock = styled.div`
-    width: 1024px;
+    width: 85%;
     margin: 0 auto;
     @media (max-width: 1376px) {
-        width: 1024px;
+        width: 85%;
     }
     @media (max-width: 1024px) {
-        width: 768px;
+        width: 90%;
     }
     @media (max-width: 768px) {
+        width: 95%;
+    }
+    @media (max-width: 584px) {
         width: 100%;
     }
 `
