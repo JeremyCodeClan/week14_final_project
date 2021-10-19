f73e44cff6a27120039b719adc82e9a85e95f7c7
Jeremy Yoo <jyyoo824@gmail.com>
2021-10-19 16:18:46 +0100

    add browserRouter in index & edit pages

--- client/src/pages/ChartPage.js
+++ client/src/pages/ChartPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from 'components/common/Layout'
 
 const ChartPage = () => {
     return (
-        <></>
+        <Layout>
+            <h1>Chart Page</h1>
+        </Layout>
     )
 }
 
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from 'components/common/Layout'
 
 const HomePage = () => {
     return (
-        <></>
+        <Layout>
+            <h1>Home Page</h1>
+        </Layout>
     )
 }
 
