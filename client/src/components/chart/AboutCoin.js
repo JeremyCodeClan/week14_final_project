da572667c1af08e6a7f1517850603af9e7793bed
Jeremy Yoo <jyyoo824@gmail.com>
2021-10-21 11:12:53 +0100

    create coin about & investinfo containers & components

---
+++ client/src/components/chart/AboutCoin.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+// import Responsive from 'components/common/Responsive';
+
+// const ChartTabBlock = styled(Responsive)`
+//     margin-top: 2rem;
+// `;
+
+const AboutCoinWrapper = styled.article`
+    /* space indicate purpose */
+    border: 1px dashed red;
+    background: var(--bright-white);
+
+    width: 50%;
+    height: 100%;
+
+`;
+
+const ContentBlock = styled.section`
+    
+    padding: 0.5rem;
+
+    .big-value {
+        font-size: var(--ft-heading);
+        padding: 0 0.5rem;
+    }
+`;
+
+
+const AboutCoin = () => {
+
+    return (
+        <AboutCoinWrapper>
+            <ContentBlock>
+                <div className="big-value">About Coin</div>
+                <div className="big-value">Description</div>
+                <div className="big-value">Link to Coin_site</div>
+            </ContentBlock>
+        </AboutCoinWrapper>
+    )
+};
+
+export default AboutCoin;
