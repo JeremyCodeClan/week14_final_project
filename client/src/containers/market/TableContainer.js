b410a4656d55b9be732a8f365085fa8966c4801f
Jeremy Yoo <jyyoo824@gmail.com>
2021-10-21 11:14:49 +0100

    edit talbe & ticker container

--- client/src/containers/market/TableContainer.js
+++ client/src/containers/market/TableContainer.js
@@ -3,15 +3,24 @@ import { useSelector } from 'react-redux';
 import TableList from 'components/market/TableList'
 
 const TableContainer = () => {
-    const { bts_usd, eth_usd } = useSelector(({ saveTickers }) => ({
-        bts_usd: saveTickers.bts_usd,
-        eth_usd: saveTickers.eth_usd,
+    const { bts_usdt, eth_usdt, sol_usdt, dot_usdt, doge_usdt } = useSelector(({ saveTickers }) => ({
+        bts_usdt: saveTickers.bts_usdt,
+        eth_usdt: saveTickers.eth_usdt,
+        sol_usdt: saveTickers.sol_usdt,
+        dot_usdt: saveTickers.dot_usdt,
+        doge_usdt: saveTickers.doge_usdt,
     }))
-    
+
     return (
         <>
-        {bts_usd !== null && eth_usd !== null ? 
-            <TableList bts_usd={bts_usd} eth_usd={eth_usd} />
+        {bts_usdt !== null && eth_usdt !== null && sol_usdt !== null && dot_usdt !== null && doge_usdt !== null ? 
+            <TableList 
+                bts_usdt={bts_usdt}
+                eth_usdt={eth_usdt}
+                sol_usdt={sol_usdt}
+                dot_usdt={dot_usdt}
+                doge_usdt={doge_usdt}
+            />
             :
             <></>
         }
